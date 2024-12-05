b98b5534ff552cd2e3a73e9583dca9503abb5da6
SaavayuAdrin <52580752+AragornFire@users.noreply.github.com>
2024-12-04 22:23:53 -0500

    yipee!

--- boolexpr1.py
+++ boolexpr1.py
@@ -5,19 +5,19 @@ from BoolExprADT import *
 import sys
 
 # EBNF grammar
-# <program>      : ( <declarations> , <expr> )
+# <program>	  : ( <declarations> , <expr> )
 # <declarations> : [ ]
 # <declarations> : [ <varlist> ]
-# <varlist>      : <var>
-# <varlist>      : <var> , <varlist>
-# <expr>         : <expr> & <expr>
-# <expr>         : <expr> | <expr>
-# <expr>         : ~ <expr>
-# <expr>         : <literal>
-# <expr>         : <var>
-# <literal>      : t
-# <literal>      : f
-# <var>          : a...z     but not t or f obviously
+# <varlist>	  : <var>
+# <varlist>	  : <var> , <varlist>
+# <expr>		 : <expr> & <expr>
+# <expr>		 : <expr> | <expr>
+# <expr>		 : ~ <expr>
+# <expr>		 : <literal>
+# <expr>		 : <var>
+# <literal>	  : t
+# <literal>	  : f
+# <var>		  : a...z	 but not t or f obviously
 
 #################### BEGIN Lexer/Scanner Specification ####################
 
@@ -66,65 +66,66 @@ precedence = (
 
 #################### BEGIN Grammar Pattern-Action Rules ####################
 
+true_vars = {}
+false_vars = {}
+
 global_ast = ""
 
 # Grammar rules
 def p_sentence(p):
-    '''sentence : LPAREN declarations COMMA expr RPAREN'''
-    global global_ast
-    p[0] = p[4]
-    global_ast = str(p[0])
+	'''sentence : LPAREN declarations COMMA expr RPAREN'''
+	global global_ast
+	p[0] = p[4]
+	global_ast = p[0]
 
 def p_declarations(p):
-    '''declarations : LBRACK RBRACK
-                    | LBRACK varlist RBRACK'''
-    pass
+	'''declarations : LBRACK RBRACK
+					| LBRACK varlist RBRACK'''
+	pass
 
 def p_varlist(p):
-    '''varlist : varlist COMMA VAR
-               | VAR'''
-    global true_vars
-    #Unsure if this works##################
-    #pass
-
-    if len(p) > 3:
-        true_vars.add(p[3])
-        p[0] = True
-    else:
-        true_vars.add(p[1])
-        p[0] = True
-    
+	'''varlist : varlist COMMA VAR
+			   | VAR'''
+	global true_vars
+	#Unsure if this works##################
+	#pass
+	if len(p) > 3:
+		true_vars[p[3]] = True
+		p[0] = True
+	else:
+		true_vars[p[1]] = True
+		p[0] = True
+	
 
 def p_expr_literal(p):
-    '''expr : T
-            | F'''
-    p[0] = p[1] in "Ttτ"
-    #p[0] = bool(p[1])
+	'''expr : T
+			| F'''
+	p[0] = Literal(value=p[1] in "Ttτ")
 
 def p_expr_not(p):
-    '''expr : NOT expr'''
-    p[0] = not p[2]
+	'''expr : NOT expr'''
+	p[0] = Operator('~', [p[2]])
 
 def p_expr_and(p):
-    '''expr : expr AND expr'''
-    if p[2] == '&':
-        p[0] = p[1] and p[3]
+	'''expr : expr AND expr'''
+	p[0] = Operator('&', [p[1], p[3]])
 
 def p_expr_or(p):
-    '''expr : expr OR expr'''
-    if p[2] == '|':
-        p[0] = p[1] or p[3]
+	'''expr : expr OR expr'''
+	p[0] = Operator('|', [p[1], p[3]])
 
 def p_expr_var(p):
-    '''expr : VAR'''
-    p[0] = p[1] in true_vars
+	'''expr : VAR'''
+	if not p[1] in true_vars:
+		false_vars[p[1]] = False
+	p[0] = Variable(p[1])
 
 # Error rule for syntax errors
 def p_error(p):
-    if p:
-        print(f"Syntax error at '{p.value}'")
-    else:
-        print("Syntax error at EOF")
+	if p:
+		print(f"Syntax error at '{p.value}'")
+	else:
+		print("Syntax error at EOF")
 
 #################### END Grammar Pattern-Action Rules ####################
 
@@ -133,17 +134,19 @@ parser = yacc.yacc()
 
 # Function for checking if a variable is false
 def false_var(var: str) -> bool:
-   return var not in true_vars
+   return var not in true_vars.keys()
 
 #print(parser.parse('([] , f | t & f | ~ t)'))
 # main entry point
-#####if __name__ == "__main__":
-#####   main()
+if __name__ == "__main__":
+	main()
 
+"""
 for i, line in enumerate(sys.stdin):
-    truevars = set()
-    line = line.strip()
-    print(f'{len(line)} -> {line}')
-    print(f'{i+1}) {parser.parse(line)}')
+	true_vars = set()
+	line = line.strip()
+	print(f'{len(line)} -> {line}')
+	print(f'{i+1}) {parser.parse(line)}')
+"""
 
 # print(parser.parse("([q], p & q | f)"))
