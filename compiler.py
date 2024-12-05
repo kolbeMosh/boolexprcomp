7f7c12b2605222a6b6ef8d4e02dd6cbf4bf68c07
SaavayuAdrin <52580752+AragornFire@users.noreply.github.com>
2024-12-04 22:58:18 -0500

    wahoo!

--- compiler.py
+++ compiler.py
@@ -1,38 +1,50 @@
+#*******************************************************************************
+#
+#      filename:  compiler.py
+#
+#   description:  Creates a C++ file that evaluates a BoolExpr
+#
+#        author:  Mosher, Kolbe and Martinez, Michael
+# AMU e-mail id:  kolbe.mosher@my.avemaria.edu, michael.martinez@my.avemaria.edu
+#
+#        course:  CSCI 370: Programming Languages
+#    instructor:  Dr. Perugini
+#    assignment:  Quiz III
+#
+#      assigned:  November 27, 2024
+#           due:  December 5, 2024
+#
+#*******************************************************************************
+
 from BoolExprADT import *
 import repl
 import boolexpr1
 
-# Function to compile a BoolExpr expression into a C++ code string
 def compile_expr(expr: BoolExpr, env: dict = None) -> None:
-	if env is None:
-		env = {}
-	if isinstance(expr, Literal):
-		repl.translated_string += 'true' if expr.value else 'false'
-	elif isinstance(expr, Variable):
-		repl.translated_string += expr.name
+	if env is None: env = {}
+	
+	if isinstance(expr, Literal): repl.translated_string += 'true' if expr.value else 'false'
+	elif isinstance(expr, Variable): repl.translated_string += expr.name
 	elif isinstance(expr, Operator):
 		if expr.operator == '&':
-			repl.translated_string += '('
+			repl.translated_string.append('(')
 			compile_expr(expr.operands[0], env)
-			repl.translated_string += ' && '
+			repl.translated_string.append(' && ')
 			compile_expr(expr.operands[1], env)
-			repl.translated_string += ')'
+			repl.translated_string.append(')')
 		elif expr.operator == '|':
-			repl.translated_string += '('
+			repl.translated_string.append('(')
 			compile_expr(expr.operands[0], env)
-			repl.translated_string += ' || '
+			repl.translated_string.append(' || ')
 			compile_expr(expr.operands[1], env)
-			repl.translated_string += ')'
+			repl.translated_string.append(')')
 		elif expr.operator == '~':
-			repl.translated_string += '!'
+			repl.translated_string.append('!')
 			compile_expr(expr.operands[0], env)
-	else:
-		raise ValueError(f"Unsupported node: {type(expr)}")
+	else: raise ValueError(f"What is this? What? Is? This?: {type(expr)}")
 
-
-# Writes compiled string translated_string to a file <lineno>.cpp
 def write_cpp_output(lineno: int) -> None:
-	#global translated_string, true_vars, false_vars
+	
 	with open(f"{lineno}.cpp", "w") as file:
 		file.write("#include <iostream>\n")
 		file.write("using namespace std;\n\n")
